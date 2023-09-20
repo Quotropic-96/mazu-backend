12843a7a9ef7858b843f4e2af0f7702fdb191500
Gerard Solanes <gerardsolher@gmail.com>
2023-09-20 12:05:44 +0200

    Adds get whales route

--- routes/whales.ts
+++ routes/whales.ts
@@ -7,8 +7,25 @@ const router = express.Router();
 // @route   GET /api/v1/whales/getAll/
 // @access  Public
 router.get("/getAll", async (req, res, next) => {
-  const whales = await Whale.find();
-  res.status(200).json(whales);
+  try {
+    const whales = await Whale.find();
+    res.status(200).json(whales);
+  } catch (error) {
+    next(error);
+  }
 });
+
+// @desc    Get single whale
+// @route   GET /api/v1/whales/:whaleId
+// @access  Public
+router.get("/:whaleId", async (req, res, next) => {
+  const { whaleId } = req.params;
+  try {
+    const whale = await Whale.findById(whaleId);
+    res.status(200).json(whale);
+  } catch (error) {
+    next(error);
+  }
+})
 
 export default router;
